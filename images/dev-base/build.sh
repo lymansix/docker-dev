f44addc3dd9d9ab818ed929dd646aa24126c328c
lymansix <zhaowangliu68@gmail.com>
2025-09-27 21:11:17 +0800

    remove some cmd

--- images/dev-base/build.sh
+++ images/dev-base/build.sh
@@ -10,23 +10,10 @@ apt-install() {
 	sudo apt-get install --no-install-recommends -y "$@"
 }
 
-pip-install() {
-	if [ "$UBUNTU_RELEASE" = "jammy"]; then
-		pip install "$1"
-	else
-		pip install --break-system-packages "$1"
-	fi
-}
-
 sudo apt-get update
 
 # Super essential tools
 apt-install tree curl ca-certificates
-
-# Going to need this a lot
-apt-install python3-pip
-
-pip-install setuptools
 
 # See readme for how to get the clipboard working.
 apt-install xclip
@@ -34,22 +21,16 @@ apt-install xclip
 # To cryptographically sign git commits
 apt-install gpg gpg-agent
 
-# Man pages on base debian image aren't installed...
-apt-install man-db
-
-# tldr for a short form man pages.
-pip-install tldr
-
-# Just gitgud
-pip-install gitgud
-
 # System info. Nethogs has a bug on trusty so just going to use iftop.
 apt-install htop iotop iftop
 
 # For dig, etc. On ubuntu focal, tzdata is also getting installed, so gotta
 # work around that.
 export DEBIAN_FRONTEND=noninteractive
-echo 'Etc/UTC' | sudo tee /etc/timezone
+ecNeeded for netstat, etc.
+apt-install net-tools
+
+# ho 'Etc/UTC' | sudo tee /etc/timezone
 apt-install dnsutils
 
 # Needed for netstat, etc.
@@ -61,20 +42,29 @@ apt-install socat
 # Packet sniffer for debugging.
 apt-install tcpflow tcpdump
 
-# Very usefull for finding issues coming from syscalls
-apt-install strace
-
 # Install bash tab completion.
 apt-install bash-completion
 
 # ssh
 apt-install openssh-client
 
+# ssh
+apt-install openssh-server
+sudo mkdir -p /var/run/sshd
+sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's@#PubkeyAuthentication yes@PubkeyAuthentication yes@' /etc/ssh/sshd_config
+sudo sed -i 's@#AuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2@AuthorizedKeysFile	.ssh/authorized_keys@' /etc/ssh/sshd_config
+sudo echo "AllowagentForwarding yes" >> /etc/ssh/sshd_config
+mkdir -p /home/lymansix/.ssh
+chmod 700 /home/lymansix/.ssh
+chown -R lymansix:lymansix /home/lymansix/.ssh
+
+# tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
 # pager better than less...
 apt-install less
-
-# useful for querying json
-apt-install jq
 
 # ping servers
 apt-install inetutils-ping
@@ -113,24 +103,6 @@ rm -rf /tmp/git-open
 
 # Required for so many languages this will simply be included by default.
 apt-install build-essential pkgconf
-
-# yes
-git clone --depth 1 https://github.com/klange/nyancat /tmp/nyancat
-cd /tmp/nyancat
-make
-sudo make install
-cd /
-rm -rf /tmp/nyancat
-
-# Mail client for testing
-apt-install swaks
-
-# get lines of code in a directory
-curl -L -o ~/tokei.tar.gz https://github.com/XAMPPRocky/tokei/releases/download/v12.1.2/tokei-x86_64-unknown-linux-gnu.tar.gz
-tar xvf ~/tokei.tar.gz -C ~/
-sudo mv ~/tokei /usr/local/bin
-sudo chown root:root /usr/local/bin/tokei
-rm ~/tokei.tar.gz
 
 # Add timestamp to history.
 echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bashrc
